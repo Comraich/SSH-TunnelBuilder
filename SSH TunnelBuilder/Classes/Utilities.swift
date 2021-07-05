ad00a92b88244c83a3e07a6863404ab039f59c70
Simon Bruce-Cassidy <simon@gale-huset.net>
2021-07-05 21:24:16 +0200

    Added Utilities class and alert box function

---
+++ SSH TunnelBuilder/Classes/Utilities.swift
@@ -0,0 +1,23 @@
+//
+//  Utilities.swift
+//  SSH TunnelBuilder
+//
+//  Created by Simon Bruce-Cassidy on 05/07/2021.
+//
+
+import AppKit
+
+class Utilities
+{
+    static func ShowAlertBox( alertStyle: NSAlert.Style, message: String )
+    {
+        DispatchQueue.main.async {
+            let alert = NSAlert()
+            alert.alertStyle = alertStyle
+            alert.messageText = message
+            alert.addButton(withTitle: "OK")
+            alert.runModal()
+            return
+        }
+    }
+}
