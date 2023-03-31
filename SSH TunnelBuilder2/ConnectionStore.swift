9d4313d9774ded3ad6969661026909e9732975f6
Simon Bruce-Cassidy <simon.bruce@soprasteria.com>
2023-03-31 20:26:00 +0200

    Working GUI with the exception of connection editing

--- SSH TunnelBuilder2/ConnectionStore.swift
+++ SSH TunnelBuilder2/ConnectionStore.swift
@@ -8,37 +8,111 @@ import Foundation
 import CloudKit
 
 class ConnectionStore: ObservableObject {
+    @Published var mode: MainViewMode = .loading
     @Published var connections: [Connection] = []
+    private let container = CKContainer.default()
+    private let publicDB = CKContainer.default().publicCloudDatabase
+    private var customZone: CKRecordZone?
+    private let customZoneName = "ConnectionZone"
     
-    private let container: CKContainer
-    private let publicDB: CKDatabase
-    
+    @Published var connectionName = ""
+    @Published var serverAddress = ""
+    @Published var portNumber = ""
+    @Published var username = ""
+    @Published var password = ""
+    @Published var privateKey = ""
+    @Published var localPort = ""
+    @Published var remoteServer = ""
+    @Published var remotePort = ""
+
     init() {
-        container = CKContainer.default()
-        publicDB = container.publicCloudDatabase
-        fetchConnections()
-    }
-    
-    func fetchConnections() {
-        let predicate = NSPredicate(value: true)
-        let query = CKQuery(recordType: "Connection", predicate: predicate)
-        
-        publicDB.perform(query, inZoneWith: nil) { [weak self] records, error in
-            if let error = error {
-                print("Error fetching connections: \(error.localizedDescription)")
-                return
-            }
-            
-            guard let records = records else { return }
-            
-            DispatchQueue.main.async {
-                self?.connections = records.compactMap { self?.recordToConnection(record: $0) }
+        createCustomZone { result in
+            switch result {
+            case .success():
+                DispatchQueue.main.async {
+                }
+                print("Created custom zone successfully")
+                self.fetchConnections(cursor: nil)
+            case .failure(let error):
+                print("Failed to create custom zone: \(error.localizedDescription)")
             }
         }
     }
+
+    func createCustomZone(completion: @escaping (Result<Void, Error>) -> Void) {
+        let customZone = CKRecordZone(zoneName: customZoneName)
+        
+        let createZoneOperation = CKModifyRecordZonesOperation(recordZonesToSave: [customZone], recordZoneIDsToDelete: nil)
+        createZoneOperation.modifyRecordZonesCompletionBlock = { savedZones, deletedZoneIDs, error in
+            if let error = error {
+                print("Error creating custom zone: \(error.localizedDescription)")
+                print("Detailed error: \(error)")
+                completion(.failure(error))
+            } else if let savedZone = savedZones?.first {
+                self.customZone = savedZone
+                completion(.success(()))
+            }
+        }
+        
+        container.privateCloudDatabase.add(createZoneOperation)
+    }
     
-    func saveConnection(_ connection: Connection) {
-        let record = CKRecord(recordType: "Connection")
+    func fetchConnections(cursor: CKQueryOperation.Cursor? = nil) {
+        let query = CKQuery(recordType: "Connection", predicate: NSPredicate(value: true))
+        query.sortDescriptors = []
+        
+        let queryOperation: CKQueryOperation
+        
+        if let cursor = cursor {
+            queryOperation = CKQueryOperation(cursor: cursor)
+        } else {
+            queryOperation = CKQueryOperation(query: query)
+        }
+        
+        queryOperation.desiredKeys = ["uuid", "name", "serverAddress", "portNumber", "username", "password", "privateKey", "localPort", "remoteServer", "remotePort"]
+        
+        queryOperation.recordFetchedBlock = { [weak self] record in
+            DispatchQueue.main.async {
+                if let connection = self?.recordToConnection(record: record) {
+                    self?.connections.append(connection)
+                }
+            }
+        }
+        
+        queryOperation.queryCompletionBlock = { (cursor, error) in
+            if let error = error {
+                print("Error fetching connections: \(error.localizedDescription)")
+            } else if let cursor = cursor {
+                print("Fetching more connections")
+                self.fetchConnections(cursor: cursor)
+            } else {
+                print("Fetched connections successfully.")
+            }
+            
+            DispatchQueue.main.async {
+                self.mode = self.connections.isEmpty ? .create : .view
+            }
+        }
+        
+        // Use the privateCloudDatabase instead of publicDB
+        CKContainer.default().privateCloudDatabase.add(queryOperation)
+    }
+
+    func createConnection(name: String, serverAddress: String, portNumber: String, username: String, password: String, privateKey: String, localPort: String, remoteServer: String, remotePort: String) {
+        let newConnection = Connection(name: name, serverAddress: serverAddress, portNumber: portNumber, username: username, password: password, privateKey: privateKey, localPort: localPort, remoteServer: remoteServer, remotePort: remotePort)
+        print("Creating connection: \(newConnection)")
+        saveConnection(newConnection)
+    }
+    
+    func saveConnection(_ connection: Connection, recordID: CKRecord.ID? = nil) {
+        let record: CKRecord
+        
+        if let recordID = recordID {
+            record = CKRecord(recordType: "Connection", recordID: recordID)
+        } else {
+            record = CKRecord(recordType: "Connection")
+        }
+
         record["uuid"] = connection.id.uuidString as CKRecordValue
         record["name"] = connection.name as CKRecordValue
         record["serverAddress"] = connection.serverAddress as CKRecordValue
@@ -50,7 +124,7 @@ class ConnectionStore: ObservableObject {
         record["remoteServer"] = connection.remoteServer as CKRecordValue
         record["remotePort"] = connection.remotePort as CKRecordValue
         
-        publicDB.save(record) { [weak self] savedRecord, error in
+        CKContainer.default().privateCloudDatabase.save(record) { [weak self] savedRecord, error in
             if let error = error {
                 print("Error saving connection: \(error.localizedDescription)")
                 return
@@ -59,7 +133,16 @@ class ConnectionStore: ObservableObject {
             guard let savedRecord = savedRecord else { return }
             
             DispatchQueue.main.async {
-                self?.connections.append((self?.recordToConnection(record: savedRecord)!)!)
+                if let connection = self?.recordToConnection(record: savedRecord) {
+                    print("Connection saved: \(connection)")
+                    if let recordID = recordID {
+                        if let index = self?.connections.firstIndex(where: { $0.recordID?.recordName == recordID.recordName }) {
+                            self?.connections[index] = connection
+                        }
+                    } else {
+                        self?.connections.append(connection)
+                    }
+                }
             }
         }
     }
@@ -67,7 +150,7 @@ class ConnectionStore: ObservableObject {
     func deleteConnection(_ connection: Connection) {
         guard let recordID = connection.recordID else { return }
         
-        publicDB.delete(withRecordID: recordID) { [weak self] deletedRecordID, error in
+        CKContainer.default().privateCloudDatabase.delete(withRecordID: recordID) { [weak self] deletedRecordID, error in
             if let error = error {
                 print("Error deleting connection: \(error.localizedDescription)")
                 return
@@ -92,6 +175,18 @@ class ConnectionStore: ObservableObject {
               let remoteServer = record["remoteServer"] as? String,
               let remotePort = record["remotePort"] as? String else {
             return nil
+        }
+        
+        if let id = record["uuid"] as? String {
+            print("Fetched uuid: \(id)")
+        } else {
+            print("Missing uuid")
+        }
+
+        if let name = record["name"] as? String {
+            print("Fetched name: \(name)")
+        } else {
+            print("Missing name")
         }
 
         return Connection(id: uuid, recordID: record.recordID, name: name, serverAddress: serverAddress, portNumber: portNumber, username: username, password: password, privateKey: privateKey, localPort: localPort, remoteServer: remoteServer, remotePort: remotePort)
--- SSH TunnelBuilder2/ContentView.swift
+++ SSH TunnelBuilder2/ContentView.swift
@@ -9,49 +9,130 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var connectionStore = ConnectionStore()
-
+    @State private var selectedConnection: Connection?
+    // @State private var mode: MainViewMode = .create
+    
+    @State private var connectionName = ""
+    @State private var serverAddress = ""
+    @State private var portNumber = ""
+    @State private var username = ""
+    @State private var password = ""
+    @State private var privateKey = ""
+    @State private var localPort = ""
+    @State private var remoteServer = ""
+    @State private var remotePort = ""
+    
     var body: some View {
         NavigationView {
-            NavigationList(connectionStore: connectionStore)
-            MainView(connectionStore: connectionStore)
+            NavigationList(connectionStore: connectionStore, selectedConnection: $selectedConnection)
+                .onChange(of: selectedConnection) { connection in
+                    if let connection = connection {
+                        connectionName = connection.name
+                        serverAddress = connection.serverAddress
+                        portNumber = connection.portNumber
+                        username = connection.username
+                        password = connection.password
+                        privateKey = connection.privateKey
+                        localPort = connection.localPort
+                        remoteServer = connection.remoteServer
+                        remotePort = connection.remotePort
+                    } else {
+                        connectionName = ""
+                        serverAddress = ""
+                        portNumber = ""
+                        username = ""
+                        password = ""
+                        privateKey = ""
+                        localPort = ""
+                        remoteServer = ""
+                        remotePort = ""
+                    }
+                }
+            MainView(connection: selectedConnection, connectionStore: connectionStore, mode: $connectionStore.mode, connectionName: $connectionName, serverAddress: $serverAddress, portNumber: $portNumber, username: $username, password: $password, privateKey: $privateKey, localPort: $localPort, remoteServer: $remoteServer, remotePort: $remotePort, selectedConnection: $selectedConnection)
         }
     }
 }
 
-
 struct NavigationList: View {
     @ObservedObject var connectionStore: ConnectionStore
+    @Binding var selectedConnection: Connection?
     
     var body: some View {
-        List {
-            ForEach(connectionStore.connections) { connection in
-                NavigationLink(destination: MainView(connectionStore: connectionStore, connection: connection)) {
-                    Text(connection.name)
-                }
+        List(connectionStore.connections) { connection in
+            NavigationLink(destination: mainViewForConnection(connection), tag: connection, selection: $selectedConnection) {
+                ConnectionRow(connection: connection, isSelected: selectedConnection == connection)
             }
         }
         .listStyle(SidebarListStyle())
-        .frame(minWidth: 220)
+        .frame(minWidth: 230)
         .navigationTitle("Navigation")
         .toolbar {
-            Button(action: {
-                // Connect action
-            }) {
-                Text("➕")
+            ToolbarItem(placement: .automatic) {
+                Button(action: {
+                    connectionStore.mode = .create
+                    selectedConnection = nil
+                }) {
+                    Image(systemName: "plus")
+                }
+                .help("Create new connection")
             }
             
-            Button(action: {
-                // Edit action
-            }) {
-                Text("📝")
+            ToolbarItem(placement: .automatic) {
+                if selectedConnection != nil {
+                    Button(action: {
+                        print("Selected connection: \(String(describing: selectedConnection ?? nil))")
+                        connectionStore.mode = .edit
+                    }) {
+                        Image(systemName: "pencil")
+                    }
+                    .help("Edit selected connection")
+                }
             }
             
-            Button(action: {
-                // Delete action
-            }) {
-                Text("❌")
+            ToolbarItem(placement: .automatic) {
+                if let selectedConnection = selectedConnection {
+                    Button(action: {
+                        // Delete action
+                        connectionStore.deleteConnection(selectedConnection)
+                        self.selectedConnection = nil
+                    }) {
+                        Image(systemName: "trash")
+                    }
+                    .help("Delete selected connection")
+                }
             }
         }
+    }
+    
+    func deleteConnection(_ connection: Connection) {
+        connectionStore.deleteConnection(connection)
+    }
+    
+    func mainViewForConnection(_ connection: Connection) -> some View {
+        MainView(connection: connection,
+                 connectionStore: connectionStore,
+                 mode: Binding.constant(.view),
+                 connectionName: Binding.constant(connection.name),
+                 serverAddress: Binding.constant(connection.serverAddress),
+                 portNumber: Binding.constant(connection.portNumber),
+                 username: Binding.constant(connection.username),
+                 password: Binding.constant(connection.password),
+                 privateKey: Binding.constant(connection.privateKey),
+                 localPort: Binding.constant(connection.localPort),
+                 remoteServer: Binding.constant(connection.remoteServer),
+                 remotePort: Binding.constant(connection.remotePort),
+                 selectedConnection: Binding.constant(connection.self))
+    }
+}
+
+struct ConnectionRow: View {
+    let connection: Connection
+    let isSelected: Bool
+    
+    var body: some View {
+        Text(connection.name)
+            .background(isSelected ? Color.blue.opacity(0.3) : Color.clear)
+            .cornerRadius(5)
     }
 }
 
@@ -65,91 +146,164 @@ enum MainViewMode {
     case create
     case edit
     case view
+    case loading
 }
 
 struct MainView: View {
-    @ObservedObject var connectionStore: ConnectionStore
     var connection: Connection?
+    @ObservedObject var connectionStore: ConnectionStore
+    @Binding var mode: MainViewMode
     
     @State private var connectionState: ConnectionState = .disconnected
     
-    @State private var connectionName = "Connection Name"
-    @State private var serverAddress = ""
-    @State private var portNumber = ""
-    @State private var username = ""
-    @State private var password = ""
-    @State private var privateKey = ""
-    @State private var localPort = ""
-    @State private var remoteServer = ""
-    @State private var remotePort = ""
+    @Binding var connectionName: String
+    @Binding var serverAddress: String
+    @Binding var portNumber: String
+    @Binding var username: String
+    @Binding var password: String
+    @Binding var privateKey: String
+    @Binding var localPort: String
+    @Binding var remoteServer: String
+    @Binding var remotePort: String
+    @Binding var selectedConnection: Connection?
     
-    var mode: MainViewMode = .view
+    init(connection: Connection? = nil, connectionStore: ConnectionStore, mode: Binding<MainViewMode>, connectionName: Binding<String>, serverAddress: Binding<String>, portNumber: Binding<String>, username: Binding<String>, password: Binding<String>, privateKey: Binding<String>, localPort: Binding<String>, remoteServer: Binding<String>, remotePort: Binding<String>, selectedConnection: Binding<Connection?>) {
+        self.connection = connection
+        self.connectionStore = connectionStore
+        _mode = mode
+        _connectionName = connectionName
+        _serverAddress = serverAddress
+        _portNumber = portNumber
+        _username = username
+        _password = password
+        _privateKey = privateKey
+        _localPort = localPort
+        _remoteServer = remoteServer
+        _remotePort = remotePort
+        _selectedConnection = selectedConnection
+    }
     
     var body: some View {
-        VStack {
-            if mode == .view {
-                Text(connectionName)
-                    .font(.largeTitle)
+        if mode == .loading {
+            Text("Loading connections...")
+                .font(.largeTitle)
+                .padding()
+        } else {
+            VStack {
+                if mode == .create {
+                    Text("New Connection")
+                        .font(.largeTitle)
+                        .padding()
+                }
+                if mode == .view {
+                    Text(connectionName)
+                        .font(.largeTitle)
+                        .padding()
+                } else {
+                    HStack {
+                        Text("Connection name:")
+                        Spacer()
+                        TextField("Enter connection name", text: $connectionName)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
                     .padding()
-            } else {
-                TextField("Enter connection name", text: $connectionName)
-                    .font(.largeTitle)
-                    .padding()
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-            }
-            
-            HStack {
-                Text("Connection Status:")
-                Spacer()
-                connectionIndicator
-                    .padding(.trailing)
-            }
-            .padding(.horizontal)
-            
-            Spacer()
-            
-            VStack(alignment: .leading) {
-                Group {
-                    infoRow(label: "Server Address:", value: $serverAddress)
-                    infoRow(label: "Port Number:", value: $portNumber)
-                    infoRow(label: "Username:", value: $username)
-                    infoRow(label: "Password:", value: $password)
-                    infoRow(label: "Private Key:", value: $privateKey)
-                    infoRow(label: "Local Port:", value: $localPort)
-                    infoRow(label: "Remote Server:", value: $remoteServer)
-                    infoRow(label: "Remote Port:", value: $remotePort)
                 }
                 
                 HStack {
-                    Button(action: {
-                        if mode == .view {
-                            // Connect action
-                        } else {
-                            // Save action
-                        }
-                    }) {
-                        Text(mode == .view ? "Connect" : "Save")
+                    if mode == .view {
+                        Text("Connection Status:")
+                        Spacer()
+                        connectionIndicator
+                            .padding(.trailing)
                     }
-                    .padding()
                     
+                }
+                .padding(.horizontal)
+                
+                if mode == .view {
                     Spacer()
                 }
-                .padding(.bottom)
+                
+                VStack(alignment: .leading) {
+                    Group {
+                        infoRow(label: "Server Address:", value: $serverAddress)
+                        infoRow(label: "Port Number:", value: $portNumber)
+                        infoRow(label: "Username:", value: $username)
+                        infoRow(label: "Password:", value: $password, isSecure: true)
+                        infoRow(label: "Private Key:", value: $privateKey, isSecure: true)
+                        infoRow(label: "Local Port:", value: $localPort)
+                        infoRow(label: "Remote Server:", value: $remoteServer)
+                        infoRow(label: "Remote Port:", value: $remotePort)
+                    }
+                    
+                    HStack {
+                        Button(action: {
+                            if mode == .create {
+                                connectionStore.createConnection(name: connectionName, serverAddress: serverAddress, portNumber: portNumber, username: username, password: password, privateKey: privateKey, localPort: localPort, remoteServer: remoteServer, remotePort: remotePort)
+                                mode = .view
+                            } else if mode == .view {
+                                // Connect action
+                            } else if mode == .edit {
+                                if let connection = connection {
+                                    let updatedConnection = Connection(id: connection.id, recordID: connection.recordID, name: $connectionName.wrappedValue, serverAddress: $serverAddress.wrappedValue, portNumber: $portNumber.wrappedValue, username: $username.wrappedValue, password: $password.wrappedValue, privateKey: $privateKey.wrappedValue, localPort: $localPort.wrappedValue, remoteServer: $remoteServer.wrappedValue, remotePort: $remotePort.wrappedValue)
+                                    connectionStore.saveConnection(updatedConnection, recordID: connection.recordID)
+                                    mode = .view
+                                }
+                            }
+                        }) {
+                            Text(mode == .create ? "Create" : mode == .view ? "Connect" : "Save")
+                        }
+                        .padding()
+                        
+                        Spacer()
+                        if mode == .edit {
+                            Text("Edit mode")
+                        }
+                    }
+                    .padding(.bottom)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .toolbar {
+                ToolbarItemGroup {
+                    if mode == .create {
+                        Button(action: {
+                            connectionStore.mode = .view
+                            if let firstConnection = connectionStore.connections.first {
+                                selectedConnection = firstConnection
+                            }
+                        }) {
+                            Text("Cancel")
+                        }
+                    }
+                }
+            }
         }
     }
-    
+
     @ViewBuilder
-    private func infoRow(label: String, value: Binding<String>) -> some View {
+    private func infoRow(label: String, value: Binding<String>, isSecure: Bool = false) -> some View {
         HStack {
             Text(label)
             Spacer()
             if mode == .view {
-                Text(value.wrappedValue)
+                if isSecure {
+                    if value.wrappedValue.isEmpty {
+                        Text("")
+                    } else {
+                        Text("<obfuscated>")
+                    }
+                } else {
+                    Text(value.wrappedValue)
+                }
             } else {
-                TextField("Enter \(label.lowercased())", text: value)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                if isSecure {
+                    SecureField("Enter \(label.lowercased())", text: value)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                } else {
+                    TextField("Enter \(label.lowercased())", text: value)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
             }
         }
         .padding(.horizontal)
@@ -186,7 +340,13 @@ struct MainView: View {
             return .orange
         }
     }
+    
+    func changeMode(to newMode: MainViewMode) {
+        self.mode = newMode
+    }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
